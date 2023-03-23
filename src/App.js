98e2fee3bc970a10b88fc88490056ce13500443b
Chris <winterch32@outlook.com>
2023-03-22 20:36:04 -0700

    CampsitesDirectoryPage and CampsiteDetail

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Header from './components/Header';
-import CampsitesList from './features/campsites/CampsitesList';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import Footer from './components/Footer';
 import './App.css';
 
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
         <Header />
-        <CampsitesList />
+        <CampsitesDirectoryPage />
         <Footer />
     </div>
   );
--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,4 +1,4 @@
-import { CAMPSTITES } from "../../app/shared/CAMPSITES";
+import { CAMPSITES } from "../../app/shared/CAMPSITES";
 
 export const selectAllCampsites = () => {
   return CAMPSITES;
