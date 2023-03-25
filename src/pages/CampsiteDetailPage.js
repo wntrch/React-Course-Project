14d4f5051e4fdfc862aa60d686c570389db37fe2
Chris <winterch32@outlook.com>
2023-03-24 17:51:52 -0700

    Comments

--- src/pages/CampsiteDetailPage.js
+++ src/pages/CampsiteDetailPage.js
@@ -1,19 +1,21 @@
 import { Container, Row } from "reactstrap";
 import { useParams } from "react-router-dom";
-import { selectCampsiteById } from '../features/campsites/campsitesSlice';
-import CampsiteDetail from '../features/campsites/CampsiteDetail';
+import { selectCampsiteById } from "../features/campsites/campsitesSlice";
+import CampsiteDetail from "../features/campsites/CampsiteDetail";
+import CommentsList from "../features/comments/CommentsList";
 
 const CampsiteDetailPage = () => {
-	const { campsiteId } = useParams();
-	const campsite = selectCampsiteById(campsiteId);
+  const { campsiteId } = useParams();
+  const campsite = selectCampsiteById(campsiteId);
 
-	return (
-		<Container>
-			<Row>
-				<CampsiteDetail campsite={campsite} />
-			</Row>
-		</Container>
-	)
+  return (
+    <Container>
+      <Row>
+        <CampsiteDetail campsite={campsite} />
+		<CommentsList campsiteId={campsiteId} />
+      </Row>
+    </Container>
+  );
 };
 
-export default CampsiteDetailPage; 
+export default CampsiteDetailPage;
