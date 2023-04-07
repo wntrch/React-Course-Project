32ba794f17bdeb5fa813ee7f772f57a3df44aea2
Chris <winterch32@outlook.com>
2023-04-06 19:44:29 -0700

    Redux campsitesSlice

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { campsitesReducer } from "../features/campsites/campsitesSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
-  },
+    campsites: campsitesReducer
+  }
 });
 
 
