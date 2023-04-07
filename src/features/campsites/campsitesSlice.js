6ff4eab30f7fd5b9d8a649fd104ce7501a6e8bde
Chris <winterch32@outlook.com>
2023-04-06 20:43:44 -0700

    Campsites useSelector

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -2,28 +2,30 @@ import { createSlice } from "@reduxjs/toolkit";
 import { CAMPSITES } from "../../app/shared/CAMPSITES";
 
 const initialState = {
-	campsitesArray: CAMPSITES
+  campsitesArray: CAMPSITES,
 };
 
 const campsitesSlice = createSlice({
-	name: "campsites",
-	initialState
+  name: "campsites",
+  initialState,
 });
 
 export const campsitesReducer = campsitesSlice.reducer;
 
-export const selectAllCampsites = () => {
-  return CAMPSITES;
+export const selectAllCampsites = (state) => {
+  return state.campsites.campsitesArray;
 };
 
-export const selectCampsiteById = (id) => {
-	return CAMPSITES.find((campsite) => campsite.id === parseInt(id));
-}
+export const selectCampsiteById = (id) => (state) => {
+  return state.campsites.campsitesArray.find(
+    (campsite) => campsite.id === parseInt(id)
+  );
+};
 
 // export const selectRandomCampsite = () => {
 // 	return CAMPSITES[Math.floor(Math.random() * CAMPSITES.length)];
 // }
 
-export const selectFeaturedCampsite = () => {
-	return CAMPSITES.find((campsite) => campsite.featured);
+export const selectFeaturedCampsite = (state) => {
+  return state.campsites.campsitesArray.find((campsite) => campsite.featured);
 };
--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -1,26 +1,33 @@
+import { useSelector } from "react-redux";
 import { Col, Row } from "reactstrap";
 // import DisplayCard from './DisplayCard';
 import AnimatedDisplayCard from "./AnimatedDisplayCard";
 import { selectFeaturedCampsite } from "../campsites/campsitesSlice";
-import { selectFeaturedPartner } from '../partners/partnersSlice';
+import { selectFeaturedPartner } from "../partners/partnersSlice";
 import { selectFeaturedPromotion } from "../promotions/promotionsSlice";
 
 const DisplayList = () => {
-	const items = [selectFeaturedCampsite(), selectFeaturedPromotion(), selectFeaturedPartner()];
+  const items = useSelector((state) => [
+    selectFeaturedCampsite(state),
+    selectFeaturedPromotion(state),
+    selectFeaturedPartner(state),
+  ]);
 
-	return (
-		<Row>
-			{items.map((item, index) => {
-				return (
-					item && (
-					<Col md className='m-1' key={index}>
-						<AnimatedDisplayCard item={item} />
-					</Col>
-					)
-				);
-			})}
-		</Row>
-	);
+  console.log("display items:", items);
+
+  return (
+    <Row>
+      {items.map((item, index) => {
+        return (
+          item && (
+            <Col md className="m-1" key={index}>
+              <AnimatedDisplayCard item={item} />
+            </Col>
+          )
+        );
+      })}
+    </Row>
+  );
 };
 
 export default DisplayList;
