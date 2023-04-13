fff82d1dadd4fbeb1d386305af889995754e0534
Chris <winterch32@outlook.com>
2023-04-12 22:47:57 -0700

    Async Campsites

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-import { Routes, Route } from 'react-router-dom';
-import ContactPage from './pages/ContactPage';
-import HomePage from './pages/HomePage';
-import Header from './components/Header';
-import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
-import Footer from './components/Footer';
-import CampsiteDetailPage from './pages/CampsiteDetailPage';
-import AboutPage from './pages/AboutPage';
-import './App.css';
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Routes, Route } from "react-router-dom";
+import ContactPage from "./pages/ContactPage";
+import HomePage from "./pages/HomePage";
+import Header from "./components/Header";
+import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
+import Footer from "./components/Footer";
+import CampsiteDetailPage from "./pages/CampsiteDetailPage";
+import AboutPage from "./pages/AboutPage";
+import { fetchCampsites } from "./features/campsites/campsitesSlice";
+import "./App.css";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCampsites());
+  }, [dispatch]);
+
   return (
     <div className="App">
-        <Header />
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='contact' element={<ContactPage />} />
-          <Route path='directory' element={<CampsitesDirectoryPage />} />
-          <Route
-              path='directory/:campsiteId'
-              element={<CampsiteDetailPage />}
-          />
-          <Route path='about' element={<AboutPage />} />
-        </Routes>
-        <Footer />
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="contact" element={<ContactPage />} />
+        <Route path="directory" element={<CampsitesDirectoryPage />} />
+        <Route path="directory/:campsiteId" element={<CampsiteDetailPage />} />
+        <Route path="about" element={<AboutPage />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
